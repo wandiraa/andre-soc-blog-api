7080b322a6177a487d3da1915ea29382771a5b16
andrerw <andre.rizkiana.w@gmail.com>
2021-05-27 12:36:49 +0700

    fix: refactor code

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,8 @@
 const devConfig = {
     jwt_key: "aCkhuP4d@mu!!",
     jwt_expiration: 360000,
-    dbConnectionString: `mongodb://localhost:27017/insignia_api`,
+    // dbConnectionString: `mongodb://localhost:27017/salt_api`,
+    dbConnectionString: `mongodb+srv://andre:4uGfJJFsZunjGqI6@cluster0.8f4qq.mongodb.net/salt_api?retryWrites=true&w=majority`,
     mongoDebug: true,
 };
 
